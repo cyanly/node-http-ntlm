d826446ee2139715b46b1bd9cd09d35884a74908
colorcyan <chaoyan@gmail.com>
2014-02-11 17:03:12 +0000

    Update httpntlm.js
    Support POST and default to httpagent

--- httpntlm.js
+++ httpntlm.js
@@ -1,7 +1,7 @@
 var async = require('async');
 var httpreq = require('httpreq');
 var ntlm = require('./ntlm');
-var HttpsAgent = require('agentkeepalive').HttpsAgent;
+var HttpsAgent = require('agentkeepalive');
 var keepaliveAgent = new HttpsAgent();
 
 exports.get = function(options, callback){
@@ -39,4 +39,41 @@ exports.get = function(options, callback){
 		}
 	], callback);
 }
+
+exports.post = function(options, callback){
+	if(!options.workstation) options.workstation = '';
+	if(!options.domain) options.domain = '';
+
+	async.waterfall([
+		function ($){
+			var type1msg = ntlm.createType1Message(options);
+
+			httpreq.get(options.url, {
+				headers:{
+					'Connection' : 'keep-alive',
+					'Authorization': type1msg
+				},
+				agent: keepaliveAgent
+			}, $);
+		},
+
+		function (res, $){
+			if(!res.headers['www-authenticate'])
+				return $(new Error('www-authenticate not found on response of second request'));
+
+			var type2msg = ntlm.parseType2Message(res.headers['www-authenticate']);
+			var type3msg = ntlm.createType3Message(type2msg, options);
+			if (!(options.request.headers))
+				options.request.headers = {};
+			options.request.headers['Connection'] = 'Close';
+			options.request.headers['Authorization'] = type3msg;
+			options.request.allowRedirects = false;
+			options.request.agent = keepaliveAgent;
+			httpreq.post(options.url, options.request, $);
+		}
+	], function(err, response){
+		callback(err, response);
+	});
+}
+
 
